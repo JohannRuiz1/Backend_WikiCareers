706a1b6cadf648eff277c2401089506276bfc14f
Johann Ruiz <johannruiz176@vt.edu>
2023-09-21 14:37:47 -0400

    Adding Jsoup to pom.xml

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +10,9 @@ public class HelloController {
 
 	@GetMapping("/")
 	public String index() {
+		String html = "<html><head><title>First parse</title></head><body><p>Parsed HTML into a doc.</p></body></html>";
+		Document doc = Jsoup.parse(html);
+
 		return "Greetings from Spring Boot!";
 	}
 
