a764d4b9c343814d01efd3232d676c1c25a698ca
Michael <mikejl3@vt.edu>
2023-09-28 10:44:38 -0400

    openAI implementation

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -4,16 +4,21 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.io.IOException;
 
 @RestController
 public class HelloController {
 
 	@GetMapping("/")
 	public String index() {
-		String html = "<html><head><title>First parse</title></head><body><p>Parsed HTML into a doc.</p></body></html>";
-		Document doc = Jsoup.parse(html);
-
-		return "Greetings from Spring Boot!";
+		try {
+            String url = "https://en.wikipedia.org/wiki/Firefighter";
+            Document doc = Jsoup.connect(url).get();
+            return doc.body().toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "Error occurred";
+        }
 	}
 
 }
