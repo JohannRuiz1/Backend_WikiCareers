904abf9563ded54024c343788e97df02c37ca410
Johann Ruiz <johannruiz176@vt.edu>
2023-09-27 20:39:01 -0400

    Update for database connection

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,19 +1,63 @@
 package com.example.demo;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.DatabaseMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
+
 
 @RestController
 public class HelloController {
 
+	@Autowired
+	private DatabaseProperties databaseProperties;
+
 	@GetMapping("/")
 	public String index() {
-		String html = "<html><head><title>First parse</title></head><body><p>Parsed HTML into a doc.</p></body></html>";
-		Document doc = Jsoup.parse(html);
-
+		connect();
 		return "Greetings from Spring Boot!";
 	}
+
+	public void connect() {
+		Connection connection = null;
+		try{
+			connection = DriverManager.getConnection(databaseProperties.getUrl(), databaseProperties.getUsername(), databaseProperties.getPassword());
+			       
+            // Get database metadata
+            DatabaseMetaData metaData = connection.getMetaData();
+
+			// TODO: Put in the application properties
+            String catalog = null;
+            String schemaPattern = "CAREERINFO"; 
+
+            // Get a ResultSet of table names using the metadata
+            ResultSet tables = metaData.getTables(catalog, schemaPattern, "%", new String[] { "TABLE" });
+
+            // Iterate through the ResultSet to print the table names
+            while (tables.next()) {
+                String tableName = tables.getString("TABLE_NAME");
+                System.out.println("Table Name: " + tableName);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            // Close the database connection
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 
 }
